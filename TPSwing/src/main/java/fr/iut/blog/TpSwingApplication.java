1b47dc889522cb01217c5f32a86db0edf5a13709
Pariel-Abadie Florian <florian.pariel-abadie@etu.iut-tlse3.fr>
2025-09-29 11:31:36 +0200

    feat: endpoint /bonjour renvoie 'Bonjour le monde !'

---
+++ TPSwing/src/main/java/fr/iut/blog/TpSwingApplication.java
@@ -0,0 +1,13 @@
+package fr.iut.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSwingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpSwingApplication.class, args);
+    }
+
+}
